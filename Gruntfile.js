312b4bed19ca54df5a3826896ce09c0d7c20f84d
Jonathan Jennings <iksose.pt3@gmail.com>
2014-03-13 11:17:22 -0500

    Added concat & ng-min

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@ var db = require('./config/dbschema');
 
 module.exports = function(grunt) {
 
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-ngmin');  
+
   grunt.registerTask('dbseed', 'seed the database', function() {
     grunt.task.run('adduser:admin:admin@example.com:secret:true');
     grunt.task.run('adduser:bob:bob@example.com:secret:false');
@@ -46,5 +49,79 @@ module.exports = function(grunt) {
       });
     });
   });
+
+
+
+
+// grunt.initConfig({
+//   pkg: grunt.file.readJSON('package.json'),
+//   dirs: {
+//     src: '/client',
+//     dest: 'dist/<%= pkg.name %>/<%= pkg.version %>',
+//   },
+//   concat: {
+//     basic: {
+//       src: ['/js/alertController.js'],
+//       dest: '<%= dirs.dest %>/basic.js',
+//     },
+//     extras: {
+//       src: ['<%= dirs.src %>/main.js', '<%= dirs.src %>/extras.js'],
+//       dest: '<%= dirs.dest %>/with_extras.js',
+//     },
+//   },
+// });
+
+
+  // All upfront config goes in a massive nested object.
+  grunt.initConfig({
+    // You can set arbitrary key-value pairs.
+    distFolder: 'dist/<%= pkg.name %>',
+    // You can also set the value of a key as parsed JSON.
+    // Allows us to reference properties we declared in package.json.
+    pkg: grunt.file.readJSON('package.json'),
+    // Grunt tasks are associated with specific properties.
+    // these names generally match their npm package name.
+    concat: {
+      // Specify some options, usually specific to each plugin.
+      options: {
+        // Specifies string to be inserted between concatenated files.
+        separator: "; \n",
+        process: function(src, filepath) {
+                        return '// Source: ' + filepath + '\n' +
+                            src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+                    }
+      },
+      // 'dist' is what is called a "target."
+      // It's a way of specifying different sub-tasks or modes.
+      dist: {
+        // The files to concatenate:
+        // Notice the wildcard, which is automatically expanded.
+        src: ['client/js/*.js'],
+        // The destination file:
+        // Notice the angle-bracketed ERB-like templating,
+        // which allows you to reference other properties.
+        // This is equivalent to 'dist/main.js'.
+        dest: '<%= distFolder %>/main.js'
+        // You can reference any grunt config property you want.
+        // Ex: '<%= concat.options.separator %>' instead of ';'
+      }
+    },
+    ngmin: {
+            compile: {
+                expand: false,
+                src: '<%= distFolder %>/main.js',
+                dest: '<%= distFolder %>/main_ngmin.js'
+            }
+        }
+  }); // The end of grunt.initConfig
+
+
+
+
+
+
+
+
+
 
 };
